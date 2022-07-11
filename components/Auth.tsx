0d486f7bdca98d5c7064fa09f841c2ab66e3a091
Zac-Zajdel <zaczajdel213@gmail.com>
2022-07-10 20:17:34 -0400

    fix: Fixed issue with router from next-auth implementation
    Fixed issue with router from next-auth implementation

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import Signin from 'pages/auth/signin'
+import { useRouter } from 'next/router'
 
 type Props = {
   children: ReactNode;
@@ -8,13 +9,19 @@ type Props = {
 
 const Auth = ({ children }: Props) => {
   const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!session) {
+      router.push({ pathname: '/auth/signin' }, undefined, { shallow: true })
+    } else {
+      router.push({ pathname: '/notes' }, undefined, { shallow: true })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session])
 
   if (!session) {
-    return (
-      <>
-        <Signin />
-      </>
-    )
+    return <Signin />
   } else {
     return <>{children}</>
   }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import Layout from 'components/Layout'
 import Auth from 'components/Auth'
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
-
 import 'styles/globals.css'
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
