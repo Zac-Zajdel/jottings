85132efa42507c371f450bb6852e4eb7b66977c3
Zac-Zajdel <zaczajdel213@gmail.com>
2022-09-11 21:09:26 -0500

    feat: Add dummy header

--- pages/jots/[id].tsx
+++ pages/jots/[id].tsx
@@ -6,6 +6,13 @@ export default function Home() {
       <div className="mx-auto max-w-6xl pb-10 lg:py-12 lg:px-8 w-[800px]">
         <div className="space-y-6 sm:px-6 lg:col-span-12 lg:px-0">
           <div className="min-w-0 flex-1">
+            <div className="my-4">
+              <input
+                type="text"
+                className="border border-gray-600 text-white text-sm rounded-lg block w-full p-2.5 bg-gray-700 focus:outline-0"
+                placeholder="Title"
+              />
+            </div>
             <div className="shadow sm:overflow-hidden sm:rounded-md">
               <Tiptap />
             </div>
