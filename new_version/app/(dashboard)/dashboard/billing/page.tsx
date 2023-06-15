57368eafb55f0d1a1c65ffc85a7eb59e502c681b
Zac-Zajdel <zaczajdel213@gmail.com>
2023-06-14 22:37:53 -0500

    Remove old version

--- new_version/app/(dashboard)/dashboard/billing/page.tsx
+++ new_version/app/(dashboard)/dashboard/billing/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { redirect } from "next/navigation"
 
 import { authOptions } from "@/lib/auth"
@@ -5,40 +7,34 @@ import { getCurrentUser } from "@/lib/session"
 import { stripe } from "@/lib/stripe"
 import { getUserSubscriptionPlan } from "@/lib/subscription"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { BillingForm } from "@/components/billing-form"
 import { DashboardHeader } from "@/components/header"
 import { Icons } from "@/components/icons"
 import { DashboardShell } from "@/components/shell"
 
-export const metadata = {
-  title: "Billing",
-  description: "Manage billing and your subscription plan.",
-}
+// export const metadata = {
+//   title: "Billing",
+//   description: "Manage billing and your subscription plan.",
+// }
 
-export default async function BillingPage() {
-  const user = await getCurrentUser()
+export default function BillingPage() {
+  // TODO - IS this what is making these server side page so slow?
+  // const user = await getCurrentUser()
 
-  if (!user) {
-    redirect(authOptions?.pages?.signIn || "/login")
-  }
+  // if (!user) {
+  //   redirect(authOptions?.pages?.signIn || "/login")
+  // }
 
-  const subscriptionPlan = await getUserSubscriptionPlan(user.id)
+  const subscriptionPlan = {}
 
   // If user has a pro plan, check cancel status on Stripe.
-  let isCanceled = false
-  if (subscriptionPlan.isPro && subscriptionPlan.stripeSubscriptionId) {
-    const stripePlan = await stripe.subscriptions.retrieve(
-      subscriptionPlan.stripeSubscriptionId
-    )
-    isCanceled = stripePlan.cancel_at_period_end
-  }
+  let isCanceled = true
+  // if (subscriptionPlan.isPro && subscriptionPlan.stripeSubscriptionId) {
+  //   const stripePlan = await stripe.subscriptions.retrieve(
+  //     subscriptionPlan.stripeSubscriptionId
+  //   )
+  //   isCanceled = stripePlan.cancel_at_period_end
+  // }
 
   return (
     <DashboardShell>
