0f65481f081f4102e0287f9ad0085848a1a8ce43
Zac-Zajdel <zaczajdel213@gmail.com>
2023-06-14 22:39:25 -0500

    Deletes old version

--- old_version/types/next-auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultSession } from 'next-auth'
-
-declare module 'next-auth' {
-  // eslint-disable-next-line no-unused-vars
-  interface Session {
-    user: User & DefaultSession['user'];
-  }
-}
