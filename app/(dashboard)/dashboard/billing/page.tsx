8cd07d14cd1d586d9c976703cdbeebd7153cd3ee
Zac-Zajdel <zaczajdel213@gmail.com>
2023-06-17 00:36:47 -0500

    Fix build error

--- app/(dashboard)/dashboard/billing/page.tsx
+++ app/(dashboard)/dashboard/billing/page.tsx
@@ -1,40 +1,23 @@
-"use client"
-
 import { redirect } from "next/navigation"
 
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
-import { stripe } from "@/lib/stripe"
-import { getUserSubscriptionPlan } from "@/lib/subscription"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { BillingForm } from "@/components/billing-form"
 import { DashboardHeader } from "@/components/header"
 import { Icons } from "@/components/icons"
 import { DashboardShell } from "@/components/shell"
 
-// export const metadata = {
-//   title: "Billing",
-//   description: "Manage billing and your subscription plan.",
-// }
+export const metadata = {
+  title: "Billing",
+  description: "Manage billing and your subscription plan.",
+}
 
-export default function BillingPage() {
-  // TODO - IS this what is making these server side page so slow?
-  // const user = await getCurrentUser()
+export default async function BillingPage() {
+  const user = await getCurrentUser()
 
-  // if (!user) {
-  //   redirect(authOptions?.pages?.signIn || "/login")
-  // }
-
-  const subscriptionPlan = {}
-
-  // If user has a pro plan, check cancel status on Stripe.
-  let isCanceled = true
-  // if (subscriptionPlan.isPro && subscriptionPlan.stripeSubscriptionId) {
-  //   const stripePlan = await stripe.subscriptions.retrieve(
-  //     subscriptionPlan.stripeSubscriptionId
-  //   )
-  //   isCanceled = stripePlan.cancel_at_period_end
-  // }
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/login")
+  }
 
   return (
     <DashboardShell>
@@ -60,12 +43,6 @@ export default function BillingPage() {
             .
           </AlertDescription>
         </Alert>
-        <BillingForm
-          subscriptionPlan={{
-            ...subscriptionPlan,
-            isCanceled,
-          }}
-        />
       </div>
     </DashboardShell>
   )
