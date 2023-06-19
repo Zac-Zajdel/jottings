a8c71c7c1bfcd7f2f04a30929e88637ef9f516f9
Zac-Zajdel <zaczajdel213@gmail.com>
2023-06-19 01:36:35 -0500

    Adjusted the sidebar positioning and fixed dark mode bug with editor dropdown

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -23,7 +23,7 @@ export default async function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
+        <div className="flex h-16 items-center justify-between py-4 px-10">
           <MainNav items={dashboardConfig.mainNav} />
           <UserAccountNav
             user={{
@@ -34,7 +34,8 @@ export default async function DashboardLayout({
           />
         </div>
       </header>
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+
+      <div className="grid flex-1 gap-12 md:grid-cols-[200px_1fr] px-10">
         <aside className="hidden w-[200px] flex-col md:flex">
           <DashboardNav items={dashboardConfig.sidebarNav} />
         </aside>
