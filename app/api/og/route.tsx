1f71eb82f4a93765c3aade1e1e9df0b2b2cfa52e
Zac-Zajdel <zaczajdel213@gmail.com>
2023-08-15 23:03:12 -0500

    Cleanup

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -94,7 +94,7 @@ export async function GET(req: Request) {
               tw="flex text-xl"
               style={{ fontFamily: "Inter", fontWeight: "normal" }}
             >
-              tx.shadcn.com
+              zaczajdel.com
             </div>
             <div
               tw="flex items-center text-xl"
