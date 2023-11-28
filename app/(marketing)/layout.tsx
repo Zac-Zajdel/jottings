2bd8c54e7d6d8acf78d619639483eaf5571fc7a7
Zac-Zajdel <zaczajdel213@gmail.com>
2023-11-28 00:25:57 -0600

    Landing Page Refresh

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import { marketingConfig } from "@/config/marketing"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
 import Link from "next/link"
+import { motion } from "framer-motion"
 
 interface MarketingLayoutProps {
   children: React.ReactNode
@@ -15,22 +18,32 @@ export default async function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <div className="border-b-[1px]">
-        <header className="container z-40 bg-background">
-          <div className="flex h-20 items-center justify-between py-6">
-            <MainNav isLogin items={marketingConfig.mainNav} />
-            <nav>
-              <Link
-                href="/login"
-                className={cn(
-                  buttonVariants({ variant: "default", size: "sm" }),
-                  "px-4"
-                )}
-              >
-                Login
-              </Link>
-            </nav>
-          </div>
-        </header>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.2,
+            delay: 0.2,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+        >
+          <header className="container z-40 bg-background">
+            <div className="flex h-20 items-center justify-between py-6">
+              <MainNav isLogin items={marketingConfig.mainNav} />
+              <nav>
+                <Link
+                  href="/login"
+                  className={cn(
+                    buttonVariants({ variant: "default", size: "sm" }),
+                    "px-4"
+                  )}
+                >
+                  Login
+                </Link>
+              </nav>
+            </div>
+          </header>
+        </motion.div>
       </div>
       <main className="flex-1">{children}</main>
       <SiteFooter />
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,23 +1,29 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
 import { cn } from "@/lib/utils"
+import { useState } from "react"
 import { motion } from "framer-motion"
+import { useTheme } from "next-themes"
 import { Icons } from "@/components/icons"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
+  const { theme } = useTheme()
+  const [activeTab, setActiveTab] = useState('jots')
+
   return (
     <>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 mx-10">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <motion.div
             className="box font-heading text-3xl sm:text-5xl md:text-6xl lg:text-8xl"
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              duration: 0.5,
-              delay: 0.1,
+              duration: 0.2,
+              delay: 0.3,
               ease: [0, 0.71, 0.2, 1.01]
             }}
           >
@@ -30,7 +36,7 @@ export default function IndexPage() {
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.5,
-              delay: 0.2,
+              delay: 0.5,
               ease: [0, 0.71, 0.2, 1.01]
             }}
           >
@@ -39,105 +45,180 @@ export default function IndexPage() {
             Streamline Tasks with a Unified Workflow and Centralized Data Hub.
           </motion.p>
 
-          <motion.p
+          <motion.div
+            className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8 mt-3"
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.5,
-              delay: 0.2,
+              delay: 0.5,
               ease: [0, 0.71, 0.2, 1.01]
             }}
           >
             <Link
               href="/login"
               className={cn(
-                "mt-3",
+                "mt-6",
                 buttonVariants({ size: "lg" })
               )}
             >
               Get Started
             </Link>
-          </motion.p>
+          </motion.div>
         </div>
       </section>
-      <section
-        id="features"
-        className="container space-y-6 py-8 md:py-12 lg:py-24"
-      >
-        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-            Features
-          </h2>
-          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-            Opt-in capabilities to facilitate the most complex multi-team projects
-          </p>
-        </div>
-        <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.project className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Projects</h3>
-                <p className="text-sm text-muted-foreground">
-                  Analyze all information in one place.
-                </p>
+
+      <section className="ml-10 mr-0 3xl:mr-10">
+        <div className="overflow-hidden py-12 xl:py-12">
+          <div className="container tab-container">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 1,
+              delay: 1,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+          >
+            <div className="mx-auto grid grid-cols-1 gap-x-8 gap-y-8 lg:grid-cols-2 items-center">
+              <div className="lg:pr-8 lg:pt-4">
+                <div className="lg:max-w-lg">
+                  <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">Features</h2>
+                  <p className="mt-6 text-xl sm:text-2xl text-muted-foreground">Opt-in capabilities to facilitate the most complex multi-team projects.</p>
+                  <div className="mt-8 relative max-w-xl lg:max-w-none">
+                    <dl className="grid grid-cols-1 auto-rows-fr text-lg leading-normal">
+                      <div
+                        className={cn(
+                          "p-2 rounded-lg",
+                          { 'bg-secondary': activeTab === 'jots' }
+                        )}
+                      >
+                        <div
+                          className="relative pl-9 pr-3 rounded-lg py-4"
+                          onMouseOver={() => setActiveTab('jots')}
+                        >
+                          <dt
+                            className={cn(
+                              "inline font-semibold",
+                              { 'bg-secondary dark:text-muted': activeTab === 'jots' }
+                            )}
+                          >
+                            <Icons.file className="h-6 w-6 icon-xl absolute left-0 top-4 text-accent-400" />
+                            Jots
+                          </dt>
+                          <dd
+                            className={cn(
+                              "inline text-muted-foreground",
+                              { 'text-muted-foreground dark:text-muted': activeTab === 'jots' }
+                            )}
+                          >
+                            &nbsp;- Empower your work with a modern editor, enhancing your ability to craft and refine Jots seamlessly.
+                          </dd>
+                        </div>
+                      </div>
+
+                      <div
+                        className={cn(
+                          "p-2 rounded-lg",
+                          { 'bg-secondary': activeTab === 'templates' }
+                        )}
+                      >
+                        <div
+                          className="relative pl-9 pr-3 py-4"
+                          onMouseOver={() => setActiveTab('templates')}
+                        >
+                          <dt
+                            className={cn(
+                              "inline font-semibold",
+                              { 'bg-secondary dark:text-muted': activeTab === 'templates' }
+                            )}
+                          >
+                            <Icons.template className="h-6 w-6 icon-xl absolute left-0 top-4 text-accent-400" />
+                            Templates
+                          </dt>
+                          <dd
+                            className={cn(
+                              "inline text-muted-foreground",
+                              { 'text-muted-foreground dark:text-muted': activeTab === 'templates' }
+                            )}
+                          >
+                            &nbsp;- Elevate your Jots with curated templates, providing a foundation for building impactful content effortlessly.
+                          </dd>
+                        </div>
+                      </div>
+
+                      <div
+                        className={cn(
+                          "p-2 rounded-lg",
+                          { 'bg-secondary': activeTab === 'teams' }
+                        )}
+                      >
+                        <div
+                          className="relative pl-9 pr-3 rounded-lg py-4"
+                          onMouseOver={() => setActiveTab('teams')}
+                        >
+                          <dt
+                            className={cn(
+                              "inline font-semibold",
+                              { 'bg-secondary dark:text-muted': activeTab === 'teams' }
+                            )}
+                          >
+                            <Icons.group className="h-6 w-6 icon-xl absolute left-0 top-4 text-accent-400" />
+                            Coming Soon...
+                          </dt>
+                          <dd
+                            className={cn(
+                              "inline text-muted-foreground",
+                              { 'text-muted-foreground dark:text-muted': activeTab === 'teams' }
+                            )}
+                          >
+                            &nbsp;Foster collaboration seamlessly, enabling you to share and collaborate on content.
+                          </dd>
+                        </div>
+                      </div>
+                    </dl>
+                  </div>
+                </div>
+              </div>
+              <div className="tab-content bg-secondary w-[48rem] overflow-hidden max-w-none rounded-lg shadow-2xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0">
+                { activeTab == 'jots' &&
+                  <>
+                    <Image
+                      className="block dark:hidden"
+                      src="/images/landing/jots-light.png"
+                      alt="Jot Template"
+                      width={1200}
+                      height={600}
+                      priority={true}
+                    />
+                    <Image
+                      className="hidden dark:block"
+                      src="/images/landing/jots-dark.png"
+                      alt="Jot Template"
+                      width={1200}
+                      height={600}
+                      priority={true}
+                    />
+                  </>
+                }
+                { activeTab == 'templates' &&
+                  <Image
+                    src={`/images/landing/templates-${theme === 'dark' ? 'dark' : 'light'}.png`}
+                    alt="Jot Template"
+                    width={1200}
+                    height={600}
+                  /> }
+                { activeTab == 'teams' &&
+                  <Image
+                    src={`/images/landing/teams-${theme === 'dark' ? 'dark' : 'light'}.png`}
+                    alt="Jot Template"
+                    width={1200}
+                    height={600}
+                  /> }
               </div>
             </div>
-          </div>
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.file className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Jots</h3>
-                <p className="text-sm text-muted-foreground">
-                  Generate notes as you work.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.listCheck className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Tasks</h3>
-                <p className="text-sm text-muted-foreground">
-                  Manage deadlines and keep focused.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.group className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Groups</h3>
-                <p className="text-sm text-muted-foreground">
-                  Share information when ready.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.permissions className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Permissions</h3>
-                <p className="text-sm text-muted-foreground">
-                  Give limited access and capabilities.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <Icons.board className="h-12 w-12" />
-              <div className="space-y-2">
-                <h3 className="font-bold">Boards</h3>
-                <p className="text-sm text-muted-foreground">
-                  Visualize overall work and progress.
-                </p>
-              </div>
-            </div>
+          </motion.div>
+          
           </div>
         </div>
       </section>
