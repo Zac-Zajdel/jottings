95b89e6cafa077ef2398d8efcd3c02b92e99592e
Zachary Zajdel <zaczajdel213@gmail.com>
2024-04-21 17:12:49 -0500

    feat: custom error page and middleware to not access /signin when signed in already

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,11 @@ export default auth((req) => {
   if (!req.auth) {
     return NextResponse.rewrite(new URL('/signin', req.url))
   }
+
+  // If user is authenticated and hitting '/signin' route, redirect them back to main screen.
+  if (req.auth && req.url.includes('/signin')) {
+    return NextResponse.redirect(new URL('/jots', req.url))
+  }
 })
 
 export const config = {
@@ -13,9 +18,13 @@ export const config = {
     "/jots/:path*",
     "/templates/:path*",
     "/settings/:path*",
+    "/signin",
   ],
-  // Temporary fix until Tailwind package is compatible with edge runtime
-  // See: https://github.com/resend/react-email/issues/1105
+
+  /**
+   * Temporary fix until Tailwind package is compatible with edge runtime.
+   * See: https://github.com/resend/react-email/issues/1105
+   */
   unstable_allowDynamic: [
     "**/node_modules/@react-email*/**/*.mjs*",
   ],
